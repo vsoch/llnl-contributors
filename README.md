d4ad07a83c07feb4dac5b3f564301263858dc933
vsoch <vsoch@users.noreply.github.com>
2021-05-15 16:02:11 -0600

    adding quick view of contributors
    Signed-off-by: vsoch <vsoch@users.noreply.github.com>

---
+++ README.md
@@ -0,0 +1,13 @@
+# LLNL Contributors
+
+This is a small project to use a subset of data from [the LLNL open source site](https://llnl.github.io/)
+combined with running [this contributors analyis tool]() to quickly answer:
+
+ - What is the breakdown of interal vs. external contributors for each repository.
+ - Within a repository, where do our contributors come from?
+ - Which repositories have opportunity to improve documentation and engagement?
+ 
+If we can pair this with a primary purpose of each repository, we might be able
+to say "This tool has a strong use case for the external community but doesn't
+seem to be well known."
+
---
+++ run.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python3
+
+# This is a small script to use data already generated in data/ to
+# create plots that break down repositories by external and internal
+# contributors.
+
+import os
+import json
+from jinja2 import Template
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+def read_json(filename):
+    with open(filename, 'r') as fd:
+        content = json.loads(fd.read())
+    return content
+
+# internal and external users file
+internal_users = read_json(os.path.join(here, "data", "intUsers.json"))
+ext_users = read_json(os.path.join(here, "data", "extUsers.json"))
+
+# Keep counts of repositories by internal/external
+repos = {}
+
+def update_users(data, key="internal"):
+    """Given a dictionary of data, update repos with user counts.
+    """
+    for user, meta in data['data'].items():
+        if "contributedLabRepositories" in meta:
+            for repo in meta["contributedLabRepositories"].get("nodes", []):
+                if repo not in repos:
+                    repos[repo] = {"external": 0, "internal": 0}
+                repos[repo][key] += 1
+
+
+update_users(internal_users, "internal")
+update_users(ext_users, "external")
+
+# Keep track of maxes
+max_external = 0
+max_internal = 0
+max_total = 0
+
+# Calculate a total
+for repo, counts in repos.items():
+    counts['total'] = counts['internal'] + counts['external']
+
+    # Update max counts
+    if counts['external'] > max_external:
+        max_external = counts['external'] 
+
+    if counts['internal'] > max_internal:
+        max_internal = counts['internal'] 
+
+    if counts['total'] > max_total:
+        max_total = counts['total'] 
+
+# Sort data based on number of total
+repos = {k: v for k, v in sorted(repos.items(), key=lambda item: item[1]['total'])}
+
+# Filter down to those with less than 100
+filtered = {}
+for repo, meta in repos.items():
+    if meta['total'] <= 100:
+        filtered[repo] = meta
+
+def count_ranges(repos, metric="total"):
+    # Count ranges - we will make custom because they are so uneven
+    ranges = {"0-10": 0, "10-20":0, "20-30": 0, "30-50": 0, "50-100": 0, "100-200": 0, "200+":0}
+    for key, count in ranges.items():
+        if "+" not in key:
+            start, end = key.split('-')
+        else:
+            start = key.strip('+')
+            end = 10000000
+        start = int(start)
+        end = int(end)
+
+        # Now add repos
+        for repo, meta in repos.items():
+            if meta[metric] >= start and meta[metric] < end:
+                ranges[key] += 1
+    return ranges            
+
+total_ranges = count_ranges(repos, "total")
+external_ranges = count_ranges(repos, "external")
+internal_ranges = count_ranges(repos, "internal")
+
+ranges = {"total": total_ranges, "external": external_ranges, "internal": internal_ranges}
+
+# Write to template
+with open("template.html", "r") as fd:
+    template = Template(fd.read())
+    
+keys = ["external", "total", "internal"]
+result = template.render(repos=repos, filtered=filtered, keys=keys, ranges=ranges)
+with open("index.html", "w") as fd:
+        fd.write(result)
